38889afa07ea826cc9b7a52282f14027abb46286
PAUL LEMARON <55991207+PaulLemaron@users.noreply.github.com>
2022-07-06 10:12:54 +0300

    This is about php data types

---
+++ secondpractical.php
@@ -0,0 +1,38 @@
+<?php
+$txt="hello";
+$str ="Tutorials Class";
+echo"This is a $str <br>";
+var_dump("$txt");
+echo strrev($str)
+?>
+
+<?php
+$str ="Tutorials Class";
+echo strlen($str)
+?>
+<html>
+    <head>
+        <title>
+    array to string
+        </title>
+    </head>
+    <body>
+    username :<input type="text" name="username" placeholder="enter name" required /><br/><br/>
+        select your favourite colors:<br/>
+        <input type="checkbox" name="checklist[]"  value="red"/>
+        Red <br>
+        <input type="checkbox" name="checklist[]"value="white"/>
+        white <br>
+        <input type="checkbox"name="checklist[]"  value="blue"/>
+        Blue <br>
+
+        Green<input type="checkbox"name="checklist[]" value="Green"/></br>
+        Black<input type="checkbox"name="checklist[]" value="Black"/></br>
+        Yellow<input type="checkbox"name="checklist[]"value="Black"/></br>
+        Bisque
+        <input type="checkbox" name="Bisque" id="Bisque">
+    </body>
+    </html>
+  
+    ?>
+
---
+++ string_man.php
@@ -0,0 +1,19 @@
+<?php
+/*
+strlen();
+strrev();
+str_word_count();
+strpos();
+str_replace();
+*/
+$txt="Hello Peter";
+echo strlen($txt);
+echo "<br>";
+echo strrev($txt);
+echo "<br>";
+echo str_word_count($txt);
+echo "<br>";
+echo strpos($txt, "eter");
+echo "<br>";
+echo str_replace("Peter", "Lemaron", $txt,)
+?>
